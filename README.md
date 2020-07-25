74c75f93e350bed469299f27ae1b55b37a51e2ed
Philip Clement <pclement803@gmail.com>
2020-07-25 14:22:13 -0600

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# pclem803.github.io
+# Personal Website
+
+Hi, this is my personal website built using github pages. You can visit it [here](https://atul-jalan.github.io).
---
+++ main.js
@@ -0,0 +1,36 @@
+const show_nav_bar = () => {
+    nav_bar = document.getElementById('page_links_mobile');
+
+    if (nav_bar.style.display != "flex"){
+        nav_bar.style.display = "flex"
+    } else {
+        nav_bar.style.display = "none"
+    }
+}
+
+const resize = () => {
+
+    if (document.body.clientWidth > 800) {
+        nav_bar = document.getElementById('page_links_mobile');
+        nav_bar.style.display = "none"
+    }
+
+    const file_name = location.href.split('/').slice(-1);
+    if (file_name == 'work.html'){
+        resize_ideal_lab();
+    }
+
+}
+
+const mounted = () => {
+    resize_ideal_lab();
+}
+
+const resize_ideal_lab = () => {
+    ideal_lab_name = document.getElementById('ideal_lab_name')
+    if (document.body.clientWidth < 1050) {
+        ideal_lab_name.textContent = 'IDEAL Lab'
+    } else {
+        ideal_lab_name.textContent = 'Integrated Design Automation Laboratory'
+    }
+}
